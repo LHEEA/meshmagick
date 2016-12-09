b5fec2f5d80a242ef4d68ef14746d1b23c339aa4
François Rongère <Francois.Rongere@ec-nantes.fr>
2016-12-09 10:19:16 +0100

    Using hash for testing file loading

--- meshmagick/tests/test_IO.py
+++ meshmagick/tests/test_IO.py
@@ -3,68 +3,78 @@
 
 from meshmagick.mmio import *
 import os
+from hashlib import md5
 
 # Loaders
 # ------------------
 
-def test_load_GDF():
-    load_GDF('meshmagick/tests/data/coque.gdf')
-
-def test_load_HST():
-    load_HST('meshmagick/tests/data/SEAREV.hst')
-
-def test_load_MAR():
-    load_MAR('meshmagick/tests/data/SEAREV.mar')
-
-def test_load_TEC():
-    load_TEC('meshmagick/tests/data/SEAREV.tec')
-
 def test_load_INP():
-    load_INP('meshmagick/tests/data/Buoys.inp')
-
-def test_load_VTU():
-    load_VTU('meshmagick/tests/data/SEAREV.vtu')
-
-def test_load_VTP():
-    load_VTP('meshmagick/tests/data/SEAREV.vtp')
+    vertices, faces = load_INP('meshmagick/tests/data/Buoys.inp')
+    mv = md5()
+    mv.update(vertices)
+    assert mv.digest() == '\xea\x97dv`\xf7F\xb1\xf85\x1e}\xd4F\x0fi'
+    mf = md5()
+    mf.update(faces)
+    assert mf.digest() == '\x1e\xf7\xca\x98\xec\x19\x86\xb2\x1b\xc8\x87$\xa7\xf4\xb2\xbb'
+    return
+    
 
 def test_load_MSH():
-    load_MSH('meshmagick/tests/data/SEAREV.msh')
+    vertices, faces = load_MSH('meshmagick/tests/data/SEAREV.msh')
+    mv = md5()
+    mv.update(vertices)
+    assert mv.digest() == 'p\x97\xc0\xeb\x87\x08?\xd8\xbb\x9d\x9e\x88\x14I\x03\xfe'
+    mf = md5()
+    mf.update(faces)
+    assert mf.digest() == 'x[p\xf4\x8b\x86*H\xf5\x06\xa8\xf2\xc7\x85\x98\xa7'
+    return
 
-def test_load_STL():
-    load_STL('meshmagick/tests/data/SEAREV.stl')
+
+def test_load_WRL():
+    vertices, faces = load_WRL('meshmagick/tests/data/submarine.wrl')
+    mv = md5()
+    mv.update(vertices)
+    assert mv.digest() == 'H\xbf\xc5=;-\xe0\xab\xa1\x8b\x04A\xe6\x99U\x96'
+    mf = md5()
+    mf.update(faces)
+    assert mf.digest() == '\x15|\xda1\x8f\xe7]\xb733v\xf6\xdd.\x05\x0f'
+    return
+
+
+def test_load_MED():
+    vertices, faces = load_MED('meshmagick/tests/data/barge.med')
+    mv = md5()
+    mv.update(vertices)
+    assert mv.digest() =='\xea{p\xb9\xf6m\xe2z\x83C\xf9\rJ=\x0f\xba'
+    mf = md5()
+    mf.update(faces)
+    assert mf.digest() =='\xb2\xa93\xf2\xf9>\xa4{jJ\xd8UbD\xe1\x88'
+    return
+
 
 def test_all_io():
     vertices, faces = load_VTP('meshmagick/tests/data/SEAREV.vtp')
+    mv = md5()
+    mv.update(vertices)
+    hash_vertices = mv.digest()
+    
+    mf = md5()
+    mf.update(faces)
+    hash_faces = mv.digest()
+    
     for (loader, writer) in extension_dict.values():
         try:
-            print writer
             writer('meshfile', vertices, faces)
             can_try_to_load = True
         except NotImplementedError:
             can_try_to_load = False
+            print writer
         
         if can_try_to_load:
             try:
-                print loader
                 loader('meshfile')
             except NotImplementedError:
                 pass
     
     os.remove('meshfile')
         
-
-
-# def test_load_write_all():
-#     vertices, faces = load_VTP('tests/data/SEAREV.vtp')
-#
-#     for (loader, writer) in extension_dict.values():
-#         try:
-#             writer('tests/data/temp.dat', vertices, faces)
-#             loader('tests/data/temp.dat')
-#         except NotImplementedError:
-#             print 'NotImplemented'
-#             pass
-#         else:
-#             print 'error'
-#             pass
