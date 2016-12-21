b946b8e11d63b96256032ae367fc05c7026eb3da
François Rongère <Francois.Rongere@ec-nantes.fr>
2016-12-21 11:45:53 +0100

    deploy

--- deploy_doc.py
+++ deploy_doc.py
@@ -8,52 +8,114 @@ from distutils.dir_util import copy_tree, remove_tree
 from tempfile import mkdtemp
 import sys
 
-repo = Repo()
-git = repo.git
 
-branches = git.branch()
-print branches
-
-branches.split()
-
-# Getting the current branch (better way to do that ?)
-for branch in branches.split('\n'):
-    if branch.startswith('*'):
-        cur_branch = branch[2:]
+def checkout_branch(repo, branch):
+    try:
+        repo.git.checkout(branch)
+    except:
+        raise RuntimeError('Unable to checkout to branch %s' % branch)
 
 
-# Checking out to master branch
-git.checkout('master')
-
-# Going to doc dir and building documentation
-chdir('doc')
-system('make clean')
-system('make html')
-
-# Copying html files into a tempdir
-tempdir = mkdtemp()
-print tempdir
-
-# TODO: aller cherche dans le conf.py le dossier build (.build ou _build)
-html_files = os.path.join(getcwd(), '.build/html')
+def stash_modifs(repo):
+    try:
+        repo.git.stash()
+    except:
+        raise RuntimeError('Unable to stash modifications')
+    
+    
+def unstash_modifs(repo):
+    try:
+        repo.git.stash('pop')
+    except:
+        raise RuntimeError('Unable to unstash modifications')
 
 
-copy_tree(html_files, tempdir)
-chdir('..')
+def sphinx_build(repo):
+    pass
 
 
-git.checkout('gh-pages')
-
-# Removing everything here
-system('rm -rf ./*')  # TODO: remplacer par des fonctions pures python
-
-copy_tree(tempdir, '.')
-
-git.commit(m='Updating documentation', a=True)
+def empty_dir():
+    pass
 
 
-# Checking out back to the current branch
-git.checkout(cur_branch)
+def copy_temp():
+    return 'path'
 
 
-print git.branch()
+def retrieve_copy_temp():
+    pass
+
+
+def commit(repo, msg):
+    pass
+
+
+def push_github(repo, remote):
+    pass
+
+
+if __name__ == '__main__':
+    
+    print("Deploying documentation")
+    print("-----------------------\n")
+    
+    repo = Repo()
+    
+    # Stash modifications
+    stash_modifs(repo)
+    
+    
+    
+
+
+
+    
+# repo = Repo()
+# git = repo.git
+#
+# branches = git.branch()
+# print branches
+#
+# branches.split()
+#
+# # Getting the current branch (better way to do that ?)
+# for branch in branches.split('\n'):
+#     if branch.startswith('*'):
+#         cur_branch = branch[2:]
+#
+#
+# # Checking out to master branch
+# git.checkout('master')
+#
+# # Going to doc dir and building documentation
+# chdir('doc')
+# system('make clean')
+# system('make html')
+#
+# # Copying html files into a tempdir
+# tempdir = mkdtemp()
+# print tempdir
+#
+# # TODO: aller cherche dans le conf.py le dossier build (.build ou _build)
+# html_files = os.path.join(getcwd(), '.build/html')
+#
+#
+# copy_tree(html_files, tempdir)
+# chdir('..')
+#
+#
+# git.checkout('gh-pages')
+#
+# # Removing everything here
+# system('rm -rf ./*')  # TODO: remplacer par des fonctions pures python
+#
+# copy_tree(tempdir, '.')
+#
+# git.commit(m='Updating documentation', a=True)
+#
+#
+# # Checking out back to the current branch
+# git.checkout(cur_branch)
+#
+#
+# print git.branch()
