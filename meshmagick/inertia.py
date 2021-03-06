ecb6f43b644b8b666c872dc9a5f230173ad96e2d
Ryan Coe <rcoe@sandia.gov>
2021-03-05 20:27:50 -0700

    add method to move cog

--- meshmagick/inertia.py
+++ meshmagick/inertia.py
@@ -99,6 +99,18 @@ class RigidBodyInertia(object):
         inertia = deepcopy(self)
         inertia.shift_at_cog()
         return inertia
+
+    def set_cog(self, cog):
+        """Set a new center of gravity.
+
+        Parameters
+        ----------
+        cog : array_like
+        The 3D coordinates of the center of gravity
+        """
+        assert len(cog) == 3
+        assert isinstance(cog, (tuple,list,np.ndarray))
+        self._cog = np.asarray(cog, dtype=np.float)
     
     def shift_at_cog(self):
         """Shift the inertia matrix internally at cog.
--- meshmagick/tests/test_inertia.py
+++ meshmagick/tests/test_inertia.py
@@ -4,6 +4,7 @@
 import meshmagick.mmio as mmio
 from meshmagick.mesh import Mesh
 from math import pi, fabs
+import pytest
 
 vertices, faces = mmio.load_VTP('meshmagick/tests/data/Cylinder.vtp')
 cylinder = Mesh(vertices, faces)
@@ -20,6 +21,22 @@ def test_cylinder_inertia():
     
     inertia = cylinder.eval_plain_mesh_inertias(rho_medium=1.)
     
-    assert fabs(inertia.xx - Ixx) < 1000
-    assert fabs(inertia.zz - Izz) < 1000
+    assert pytest.approx(Ixx, rel=1e-1) == inertia.xx
+    assert pytest.approx(Izz, rel=1e-1) == inertia.zz
+
+def test_move_cog():
+    xmin, xmax, ymin, ymax, zmin, zmax = cylinder.axis_aligned_bbox
+    R = (xmax-xmin)/2.
+    h = zmax-zmin
     
+    V = pi*R**2*h
+    
+    Ixx = V*(R**2+h**2/3)/4
+    Ixx_base = Ixx + V*(h/2)**2             # parallel axis theorem
+    Ixx_base2 = 1/4*V*R**2 + 1/3*V*h**2
+
+    inertia = cylinder.eval_plain_mesh_inertias(rho_medium=1.)
+    inertia.set_cog((0,0,-h/2))
+    inertia.shift_at_cog()
+
+    assert pytest.approx(Ixx_base, rel=1e-1) == inertia.xx
