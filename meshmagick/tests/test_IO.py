a52b6bf82b50d80b6d56c73c493d90d9c2b93113
Chaitanya Kesanapalli <7126460+ckesanapalli@users.noreply.github.com>
2022-11-22 16:38:16 -0600

    Added load_gdf_compressed and write_gdf kwargs

--- meshmagick/tests/test_IO.py
+++ meshmagick/tests/test_IO.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 #  -*- coding: utf-8 -*-
+from pathlib import Path
 
 from meshmagick.mmio import *
+from meshmagick.inertia import sphere
 import os
 
 
@@ -22,3 +24,22 @@ def test_all_io():
                 pass
     
     os.remove('meshfile')
+
+
+def test_load_gdf_compressed():
+    
+    body = sphere(10)
+    vertices, faces = load_VTP('meshmagick/tests/data/SEAREV.vtp')
+    body_path = Path("temp_mesh.gdf")
+    write_GDF(body_path, vertices, faces, ulen=1, gravity=9.81, isx=0, isy=0)
+
+    gdf_vertices, gdf_faces = load_GDF(str(body_path))
+    gdf_compressed_vertices, gdf_compressed_faces = load_GDF_compressed(str(body_path))
+    
+    np.testing.assert_allclose(
+        gdf_vertices[gdf_faces], 
+        gdf_compressed_vertices[gdf_compressed_faces]
+        )
+
+    body_path.unlink()
+    
